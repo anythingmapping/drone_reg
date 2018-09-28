3b3403b751627ce314eb5e868ca5d00956115540
EC2 Default User <ec2-user@ip-172-31-26-103.eu-west-1.compute.internal>
2018-09-28 10:13:40 +0000

    finished models.py

--- collection/admin.py
+++ collection/admin.py
@@ -3,8 +3,13 @@ from collection.models import Drone, Camera
 
 class drone_admin(admin.ModelAdmin):
     model = Drone
-    list_display = ('name', 'description')
+    list_display = ('name', 'brand')
+
+class camera_admin(admin.ModelAdmin):
+    model = Camera
+    list_display = ('name', 'brand')
 
 
 # Register your models here.
-admin.site.register(Camera)
+admin.site.register(Camera, camera_admin)
+admin.site.register(Drone, drone_admin)
--- collection/models.py
+++ collection/models.py
@@ -2,14 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Drone(models.Model):
+    BRAND = (('DJI','DJI'),('PHANTOM','PHANTOM'),('x4303','x4303'))
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    brand = models.CharField(max_length=21, choices=BRAND)
+    serial_number = models.TextField(max_length=120)
+    #PLACE HOLDER FOR CAMERA MODEL LOOKUP
+    associated_camera = models.IntegerField() 
     
 class Camera(models.Model):
     """ camera options """
-    BRAND = (('hero3'),('12MP'),('gopro'))
-    
+    BRAND = (('hero3','hero3'),('12MP','12MP'),('gopro','gopro'))
     name = models.CharField(max_length=60)
-    brand = models.CharField(max_length=1, choices=BRAND)
-    megapixel = models.IntegerField(max_length=40)
+    brand = models.CharField(max_length=21, choices=BRAND)
+    megapixel = models.IntegerField()
     
